b35645f2cd07e30334c8ad6962284180de3e4778
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2025-02-10 21:54:45 +0530

    Base Code for new layout

--- app/(routes)/components/page.tsx
+++ app/(routes)/components/page.tsx
@@ -1,21 +1,12 @@
 "use client"
 
-import Dropdown from '@/app/_components/dropdown/Dropdown'
-import CodeTemplate from '@/app/_components/layouts/CodeTemplate'
-import SideMenu from '@/app/_components/side-menu/SideMenu'
 import React from 'react'
 
 const components = () => {
   return (
    <>
    <div className='px-7 py-6'>
-    <div className='flex justify-center mt-5'>
-      <Dropdown/>
-    </div>
-    <div className='flex mt-5'>
-      <SideMenu/>
-      <CodeTemplate/>
-    </div>
+    Components page 
    </div>
    </>
   )
